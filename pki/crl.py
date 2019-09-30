9401bd96bde44acf7fad1fcd359403f5ca57562b
smlu <smlu@s5.net>
2019-09-30 14:43:21 +0200

    Refactor CRL

--- pki/crl.py
+++ pki/crl.py
@@ -7,10 +7,13 @@
 
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy import create_engine
-from structure.storageManager import Connection
-from asn1crypto import x509, crl
+
+from .x509 import CscaCertificate
+from pymrtd.data.storage.storageManager import Connection
+from pymrtd.settings import *
+
 from asn1crypto.crl import CertificateList
-from settings import *
+
 import datetime
 import enum
 import pickle
@@ -23,7 +26,7 @@ CRL: \
     -authority key (CSCA - foreign key) ***
     -countrKey ***
     -start, end valid ***
-    -signiture algorithm string**
+    -signature algorithm string**
     -signature hash algorithm string**
     -SHA256 hash over whole object string or bytes
 """
@@ -31,94 +34,99 @@ CRL: \
 class CertificationRevocationListError(Exception):
     pass
 
-class SignatureAlgorithm(enum.Enum):
-    Alg1 = 1
-    Alg2 = 2
-    Alg3 = 3
+#class SignatureAlgorithm(enum.Enum):
+#    Alg1 = 1
+#    Alg2 = 2
+#    Alg3 = 3
 
-class CertificationRevocationList(CertificateList):
+class CertificateRevocationList(CertificateList):
     """Class; object that stores Certificate Revocation List (CRL) and has supporting functions"""
-    __tablename__ = 'CertificationRevocationList'
+    #__tablename__ = 'CertificationRevocationList'
 
-    crlObj = None #Column(String)
-    hashOfCrlObj = Column(String)
-    countryName = Column(String, primary_key=True)
-    size = Column(Integer)
-    validStart = Column(DateTime)
-    validEnd = Column(DateTime)
-    signatureAlgorithm = Column(String)
-    signatureHashAlgorithm = Column(String)
+    #crlObj = None #Column(String)
+    #hashOfCrlObj = Column(String)
+    #countryName = Column(String, primary_key=True)
+    #size = Column(Integer)
+    #validStart = Column(DateTime)
+    #validEnd = Column(DateTime)
+    #signatureAlgorithm = Column(String)
+    #signatureHashAlgorithm = Column(String)
 
-    def __init__(self, crl: crl):
-        """With initialization crl needs to be provided"""
-        self.crlObj = crl
-        self.hashOfCrlObj = self.calculateHashOfObj(crl)
-        self.countryName = crl.issuer.native['country_name']
-        self.size = len(crl['tbs_cert_list']['revoked_certificates'])
-        self.validStart = crl['tbs_cert_list']['this_update']
-        self.validEnd = crl['tbs_cert_list']['next_update']
-        self.signatureAlgorithm = crl['tbs_cert_list']['signature']['algorithm']
-        self.signatureHashAlgorithm = self.calculateHashOfSignatureAlgorithm(crl['tbs_cert_list']['signature']['algorithm'])
+    #def __init__(self, crl: crl):
+    #    """With initialization crl needs to be provided"""
+    #    self.crlObj = crl
+    #    self.hashOfCrlObj = self.calculateHashOfObj(crl)
+    #    self.countryName = crl.issuer.native['country_name']
+    #    self.size = len(crl['tbs_cert_list']['revoked_certificates'])
+    #    self.validStart = crl['tbs_cert_list']['this_update']
+    #    self.validEnd = crl['tbs_cert_list']['next_update']
+    #    self.signatureAlgorithm = crl['tbs_cert_list']['signature']['algorithm']
+    #    self.signatureHashAlgorithm = self.calculateHashOfSignatureAlgorithm(crl['tbs_cert_list']['signature']['algorithm'])
 
 
-    def calculateHashOfObj(self, obj: crl) -> str:
-        """Calculate SHA256 hash of whole CRL object"""
-        logger.info("Calculated value of CRL object")
-        return "TODO; calculate hash"
+    #def calculateHashOfSignatureAlgorithm(self, signatureAlgorithm: CscaCertificate) -> str:
+    #    """Calculate hash of signature algorithm"""
+    #    logger.debug("Calculated value of signature algorithm")
+    #    raise NotImplementedError()
 
-    def calculateHashOfSignatureAlgorithm(self, signatureAlgorithm: str) -> str:
-        """Calculate hash of signature algorithm"""
-        logger.info("Calculated value of signature algorithm")
-        return "TODO; calculate hash"
+    def verify(self, issuer: CscaCertificate) ->bool:
+        """Function that check if crl is signed by provided CSCA"""
+        raise NotImplementedError()
 
-    def verify(self, issuer: x509.Certificate) ->bool:
-        """Verify CRL"""
-
-    def countryName(self) -> str:
+    @property
+    def issuerCountry(self) -> str:
         """Function returns country of CRL issuer """
-        logger.info("Getting country of CRL issuer: " + self.countryName)
-        return self.countryName
+        country = self.issuer.native['country_name']
+        logger.debug("Getting country of CRL issuer: " + country)
+        return country
 
+    @property
     def size(self) -> int:
         """Function returns size of CRL"""
-        logger.info("Getting size of CRL: " + self.size)
-        return self.size
+        size = len(self['tbs_cert_list']['revoked_certificates'])
+        logger.debug("Getting size of CRL: " + size)
+        return size
 
-    def verification(self, certificateCSCA: str) -> bool:
-        """Function that check if crl is signed by provided CSCA"""
-        logger.info("Doing verification in CRL")
-        return True
-
-    def validStart(self) -> datetime:
+    @property
+    def thisUpdate(self) -> datetime:
         """In certificate the field is 'this_update'"""
-        logger.info("CRL has been created: " + self.validStart)
-        return self.validStart
+        this_update = self['tbs_cert_list']['this_update'].native
+        logger.debug("CRL has been created on: " + str(this_update))
+        return this_update
 
-    def validEnd(self) -> datetime:
+    @property
+    def nextUpdate(self) -> datetime:
         """In certificate the field is 'next_update'"""
-        logger.info("CRL will be expired: " + self.validEnd)
-        return self.validEnd
+        next_update = self['tbs_cert_list']['next_update'].native
+        logger.debug("Next CRL update: " + str(next_update))
+        return next_update
 
+    @property
     def signatureAlgorithm(self) -> str:
         """It returns signature algorithm"""
-        logger.info("Signature algorithm: " + self.signatureAlgorithm)
-        return self.signatureAlgorithm
+        sig_algo = self['signature_algorithm'].signature_algo
+        logger.debug("Signature algorithm: " + sig_algo)
+        return sig_algo
 
+    @property
     def signatureHashAlgorithm(self) -> str:
         """It returns hash of signature algorithm"""
-        logger.info("Signature hash algorithm: " + self.signatureHashAlgorithm)
-        return self.signatureHashAlgorithm
-
-    def hashOfCRLobj(self) -> str:
+        hash_algo = self['signature_algorithm'].hash_algo 
+        logger.debug("Signature hash algorithm: " + hash_algo)
+        return hash_algo
+        
+    @property
+    def fingerprint(self) -> str:
         """SHA256 hash over CRL object"""
-        logger.info("Hash of CRL object: " + self.hashOfCrlObj)
-        return self.hashOfCrlObj
+        fp = self.sha256.hex()
+        logger.debug("Fingerprint of CRL object: " + fp)
+        return fp
 
 
 #
 #Storage management functions
 #
-def write(crl: CertificationRevocationList, connection: Connection):
+def write(crl: CertificateRevocationList, connection: Connection):
     """Write to database with ORM"""
     try:
         logger.info("Writing CRL object to database. Country:" + crl.countryName())
