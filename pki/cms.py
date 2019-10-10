f363aafbee7f1272fe15a71fd2dee3bfd30aca8e
smlu <smlu@s5.net>
2019-10-10 23:46:51 +0200

    Add class SignedData && Refactor class CscaMasterList

---
+++ pki/cms.py
@@ -0,0 +1,154 @@
+from asn1crypto import algos, cms
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
+from pymrtd.pki import algo_utils, cert_utils, x509
+
+from typing import List, NoReturn, Optional, Union
+
+class SignedDataError(Exception):
+    pass
+
+class SignedData(cms.SignedData):
+    _certificate_type = x509.Certificate
+    cms.CertificateSet._child_spec = _certificate_type
+    CertList = Union[List[_certificate_type], cms.CertificateSet]
+
+    def __init__(self, value=None, default=None, **kwargs):
+        cms.CertificateSet._child_spec = SignedData._certificate_type
+        SignedData.CertList = Union[List[SignedData._certificate_type], cms.CertificateSet]
+        super().__init__(value, default, **kwargs)
+
+    @property
+    def content(self):
+         return self['encap_content_info']['content'].parsed
+
+    @property
+    def contentType(self):
+         return self['encap_content_info']['content_type']
+
+    @property
+    def certificates(self) -> CertList :
+        ''' Returns the list of certificates which signed signers info. '''
+        return self['certificates']
+
+    @property
+    def digestAlgorithms(self) -> cms.DigestAlgorithms:
+        return self['digest_algorithms']
+
+    @property
+    def signerInfos(self) -> cms.SignerInfos:
+        ''' Returns SignerInfos object. '''
+        return self['signer_infos']
+
+    @property
+    def version(self) -> cms.CMSVersion:
+        return self['version']
+
+    def getCertificateBySNI(self, sni: cms.IssuerAndSerialNumber) -> _certificate_type:
+        ''' Returns signer certificate identified by serial number and issuer '''
+        return SignedData._get_signer_cert_by_sni(self.certificates, sni)
+
+    def getCertificateByKeyId(self, keyId: bytes)  -> _certificate_type:
+        ''' Returns signer certificate identified by subject key identifier '''
+        return SignedData._get_signer_cert_by_keyid(self.certificates, keyId)
+
+    def getHasherBySidx(self, sidx) -> hashes.Hash:
+        ''' Returns hashes.Hash object specified in SignerInfo returned from SignerInfos list by its index. '''
+
+        si = self.signerInfos[sidx] 
+        hash_algo = si['digest_algorithm']['algorithm'].native
+        h = algo_utils.get_hash_algo_by_name(hash_algo)
+        return hashes.Hash(h, backend=default_backend())
+
+    def getSignatureBySidx(self, sidx) -> bytes:
+        si = selfsignerInfos[sidx]
+        return si['signature'].native
+
+    def getSignedAttributesBySidx(self, sidx) -> cms.CMSAttributes:
+        si = self.signerInfos[sidx]
+        return si['signed_attrs']
+
+    def getSigAlgoBySidx(self, sidx) -> algos.SignedDigestAlgorithm:
+        ''' Returns SignedDigestAlgorithm specified in SignerInfo returned from SignerInfos list by its index. '''
+
+        si = self.signerInfos[sidx] 
+        hash_algo = si['digest_algorithm']['algorithm'].native
+        sig_algo  = si['signature_algorithm']
+        return algo_utils.update_sig_algo_if_no_hash_algo(sig_algo, hash_algo)
+
+    def verify(self, certificateList: Optional[CertList] = []) -> NoReturn:
+        ''' 
+        Verifies every SignerInfo object and the digital signature over content.
+        On failure SignedDataError exception is risen.
+        @param (Optional) List of signing certificates
+        '''
+
+        for sidx, si in enumerate(self.signerInfos):
+            if si['version'].native == 'v1':
+                sni = si['sid'].chosen
+                c = self.getCertificateBySNI(sni)
+                if c is None:
+                    c = SignedData._get_signer_cert_by_sni(certificateList, sni)
+            elif si['version'].native == 'v3':
+                keyid = si['sid'].native
+                c = self.getCertificateByKeyId(keyid)
+                if c is None:
+                    c = SignedData._get_signer_cert_by_keyid(certificateList, keyid)
+            else:
+                raise SignedDataError("Invalid SignerInfo version at sidx: {}".format(sidx))
+
+            if c is None:
+                raise SignedDataError("Signer Certificate not found")
+            
+            if 'signed_attrs' not in si:
+                raise SignedDataError("Missing field 'signed_attrs' in signer infos")
+            sa = si['signed_attrs']
+
+            # Verify content
+            md = None
+            sig_time = None
+            for a in sa:
+                if a['type'].native == 'message_digest':
+                    md = a['values'][0].native
+                elif a['type'].native == 'signing_time':
+                    sig_time = a['values'][0].native
+                elif a['type'].native == 'content_type':
+                    ct = a['values'][0]
+                    if ct != self.contentType:
+                        raise SignedDataError("signed content type doesn't match actual content type")
+
+            if md is None:
+                raise SignedDataError("Missing 'message_digest' signed attribute")
+
+            if sig_time is not None and  not c.isValidOn(sig_time):
+                raise SignedDataError("Invalid signing time")
+
+            h = self.getHasherBySidx(sidx)
+            h.update(self.content.dump())
+            if h.finalize() != md:
+                raise SignedDataError("Content digest doesn't match signed digest")
+
+            # Make sure sa is asn1 SET type (DER tag 0x31)
+            sa.tag    = 17
+            sa.method = 1
+            sa.class_ = 0
+
+            signature = si['signature'].native
+            sig_algo  = self.getSigAlgoBySidx(sidx)
+            if not cert_utils.verify_sig(c, sa.dump(force=True), signature, sig_algo):
+                raise SignedDataError("Signature verification failed")
+
+
+    def _get_signer_cert_by_sni(cert_list: CertList, sni: cms.IssuerAndSerialNumber):
+        for c in cert_list:
+            if c.serial_number == sni['serial_number'].native and c.issuer == sni['issuer']:
+                return c
+        return None
+
+    def _get_signer_cert_by_keyid(cert_list: CertList, keyid: bytes):
+        for c in cert_list:
+            if c.key_identifier == keyid:
+                return c
+        return None
--- pki/ml.py
+++ pki/ml.py
@@ -1,12 +1,13 @@
 from asn1crypto import cms
-from asn1crypto.algos import SignedDigestAlgorithm
 import asn1crypto.core as asn1
 
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-
-from pymrtd.pki import algo_utils, cert_utils
 from .x509 import CscaCertificate, MasterListSignerCertificate
+from .cms import SignedData, SignedDataError
+
+from typing import NoReturn
+
+
+id_icao_cscaMasterList = asn1.ObjectIdentifier('2.23.136.1.1.2')  # ICAO 9303-12-p26
 
 
 class CertList(asn1.SetOf):
@@ -18,6 +19,7 @@ class CscaList(asn1.Sequence):
         ('certList', CertList)
     ]
 
+    @property
     def version(self) -> int:
         return super().__getitem__('version').native
 
@@ -37,128 +39,50 @@ class CscaList(asn1.Sequence):
 class CscaMasterListError(Exception):
     pass
 
-
 class CscaMasterList():
-    def __init__(self, cms_bytes):
-        self._cms = cms.ContentInfo.load(cms_bytes)
+    cms.ContentType._map[id_icao_cscaMasterList.dotted] = 'icaoCscaMasterList'
+    cms.EncapsulatedContentInfo._oid_specs['icaoCscaMasterList'] = CscaList
 
-        ctype = self._cms['content_type'].native
+    def __init__(self, cms_bytes):
+        SignedData._certificate_type = MasterListSignerCertificate
+        ci = cms.ContentInfo.load(cms_bytes)
+
+        ctype = ci['content_type'].native
         if ctype != 'signed_data': # ICAO 9303-12-p25
             raise CscaMasterListError("Invalid master list content type: {}, should be 'signed_data'".format(ctype))
 
-        cver = self._cms['content']['version'].native
+        self._sd = ci['content']
+        cver = self._sd.version.native
         if cver != 'v3': # ICAO 9303-12-p25
             raise CscaMasterListError("Invalid SignedData version: {}, should be 'v3'".format(cver))
 
-        econt_type = self._cms['content']['encap_content_info']['content_type'].native
-        if econt_type != '2.23.136.1.1.2': # ICAO 9303-12-p26
-            raise CscaMasterListError("Invalid encapContentInfo type: {}, should be '2.23.136.1.1.2'".format(econt_type))
+        if self._sd.contentType != id_icao_cscaMasterList:
+            raise CscaMasterListError("Invalid encapContentInfo type: {}, should be '{}'".format(econt_type, id_icao_cscaMasterList.dotted))
 
-        econt        = self._cms['content']['encap_content_info']['content'].native
-        self._cscal  = CscaList.load(econt)
-        if self._cscal.version() != 0: # ICAO 9303-12-p27
-            raise CscaMasterListError("Unsupported encapContentInfo version: {}, should be 0".format(self._cscal.version()))
+        if self._sd.content.version != 0: # ICAO 9303-12-p27
+            raise CscaMasterListError("Unsupported encapContentInfo version: {}, should be 0".format(self._sd.version))
         
-        self._mls_certs = []
-        certs = self._cms['content']['certificates']
-        if len(certs) < 1:
-            raise CscaMasterListError("No master list signing certificate found") 
+        c = self._sd.certificates[0]
+        if len(self._sd.certificates) < 1:
+            raise CscaMasterListError("No master list signer certificate found") 
 
-        for cc in certs:
-            cc.chosen.__class__ = MasterListSignerCertificate
-            self._mls_certs.append(cc.chosen)
-
-    def getSignerCertificates(self):
+    @property
+    def signerCertificates(self):
         ''' Returns the list of Master List Signer certificates. '''
-        return self._mls_certs
+        return self._sd.certificates
 
-    def getSignerCertificateBySN(self, serialNum):
-        for c in self._mls_certs:
-            if c.serial_number == serialNum:
-                return c
-        raise CscaMasterListError("Signer certificate not found")
-
-    def getSignerCertificateByKeyId(self, keyId: bytes):
-        for c in self._mls_certs:
-            if c.key_identifier == keyId:
-                return c
-        raise CscaMasterListError("Signer certificate not found")
-
-    def getCscaList(self):
+    @property
+    def cscaList(self) -> CscaList:
         ''' Returns list of CSCAs '''
-        return self._cscal
+        return self._sd.content
 
-    def getHasherBySidx(self, sidx):
-        ''' Returns hashes.Hash object specified in SignerInfo returned from SignerInfos list by its index. '''
-
-        si = self._cms['content']['signer_infos'][sidx] 
-        hash_algo = si['digest_algorithm']['algorithm'].native
-        h = algo_utils.get_hash_algo_by_name(hash_algo)
-        return hashes.Hash(h, backend=default_backend())
-
-    def getSignatureBySidx(self, sidx):
-        si = self._cms['content']['signer_infos'][sidx]
-        return si['signature'].native
-
-    def getSignedAttributesBySidx(self, sidx):
-        si = self._cms['content']['signer_infos'][sidx]
-        return si['signed_attrs']
-
-    def getSigAlgoBySidx(self, sidx):
-        ''' Returns SignedDigestAlgorithm specified in SignerInfo returned from SignerInfos list by its index. '''
-
-        si = self._cms['content']['signer_infos'][sidx] 
-        hash_algo = si['digest_algorithm']['algorithm'].native
-        sig_algo  = si['signature_algorithm']
-        return algo_utils.update_sig_algo_if_no_hash_algo(sig_algo, hash_algo)
-
-    def verify(self):
+    def verify(self) -> NoReturn:
         ''' 
         Verifies every SignerInfo object and the digital signature over content.
-        On failure CscaMasterListError exception is risen.
+        On verification failure a CscaMasterListError exception is risen.
         '''
 
-        for sidx, si in enumerate(self._cms['content']['signer_infos']):
-            if si['version'].native == 'v1':
-                c = self.getSignerCertificateBySN(si['sid'].native['serial_number'])
-            elif si['version'].native == 'v3':
-                c = self.getSignerCertificateByKeyId(si['sid'].native)
-            else:
-                raise CscaMasterListError("Invalid SignerInfo version at sidx: {}".format(sidx))
-            
-            if 'signed_attrs' not in si:
-                raise CscaMasterListError("Missing field 'signed_attrs' in signer infos")
-            sa = si['signed_attrs']
-
-            # Verify content
-            md = None
-            sig_time = None
-            for a in sa:
-                if a['type'].native == 'message_digest':
-                    md = a['values'][0].native
-                elif a['type'].native == 'signing_time':
-                    sig_time = a['values'][0].native
-
-            if md is None:
-                raise CscaMasterListError("Missing 'message_digest' signed attribute")
-
-            if sig_time is None:
-                raise CscaMasterListError("Missing 'signing_time' signed attribute")
-
-            if not c.isValidOn(sig_time):
-                raise CscaMasterListError("Invalid signing time")
-
-            h = self.getHasherBySidx(sidx)
-            h.update(self._cscal.dump())
-            if h.finalize() != md:
-                raise CscaMasterListError("Content's digest doesn't match signed digest")
-
-            # Make sure sa is asn1 SET type (DER tag 0x31)
-            sa.tag    = 17
-            sa.method = 1
-            sa.class_ = 0
-
-            signature = si['signature'].native
-            sig_algo  = self.getSigAlgoBySidx(sidx)
-            if not cert_utils.verify_sig(c, sa.dump(force=True), signature, sig_algo):
-                raise CscaMasterListError("Signature verification failed")
+        try:
+            self._sd.verify()
+        except SignedDataError as e:
+            raise CscaMasterListError(str(e)) from e
