469a1abda0147cfb9d6ce8a3ae0541a2052ba4a9
smlu <smlu@s5.net>
2019-10-12 02:31:48 +0200

    Add module ef.sod.py and ef

---
+++ ef/__init__.py
@@ -0,0 +1,22 @@
+from .sod import (
+    SOD
+)
+
+from .base import (
+    ElementaryFile,
+    LDSVersionInfo
+)
+
+from .dg import (
+    DataGroup,
+    DataGroupNumber,
+    DG15
+)
+
+__all__ = [
+    "DataGroup",
+    "DataGroupNumber",
+    "DG15",
+    "ElementaryFile",
+    "SOD"
+]
