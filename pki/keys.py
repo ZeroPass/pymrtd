2df6b37f497ee12b163703a18b6c9f4b406a61de
smlu <smlu@s5.net>
2019-10-20 21:56:48 +0200

    Add SignatureAlgorithmId and SignatureAlgorithm

--- pki/keys.py
+++ pki/keys.py
@@ -1,7 +1,71 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa, ec as ecc
-import pymrtd.pki.iso9796e2 as iso9796e2
+
+from pymrtd.pki import iso9796e2, oids
+
+from asn1crypto import algos
+
+class SignatureAlgorithmId(algos.SignedDigestAlgorithmId):
+    _map = dict(algos.SignedDigestAlgorithmId._map, **{
+        oids.ecdsa_plain_SHA1   : 'sha1_plain_ecdsa',
+        oids.ecdsa_plain_SHA224 : 'sha224_plain_ecdsa',
+        oids.ecdsa_plain_SHA256 : 'sha256_plain_ecdsa',
+        oids.ecdsa_plain_SHA384 : 'sha384_plain_ecdsa',
+        oids.ecdsa_plain_SHA512 : 'sha512_plain_ecdsa'
+    })
+
+    _reverse_map = dict(algos.SignedDigestAlgorithmId._reverse_map, **{
+        'sha1_plain_ecdsa'   : oids.ecdsa_plain_SHA1,
+        'sha224_plain_ecdsa' : oids.ecdsa_plain_SHA224,
+        'sha256_plain_ecdsa' : oids.ecdsa_plain_SHA256,
+        'sha384_plain_ecdsa' : oids.ecdsa_plain_SHA384,
+        'sha512_plain_ecdsa' : oids.ecdsa_plain_SHA512
+    })
+
+
+class SignatureAlgorithm(algos.SignedDigestAlgorithm):
+    _fields  = [('algorithm', SignatureAlgorithmId), *algos.SignedDigestAlgorithm._fields[1:]]
+
+    @property
+    def isPlain(self):
+        return 'plain' in self['algorithm'].native
+
+    @property
+    def signatureAlgo(self):
+        if not self.isPlain:
+            return super().signature_algo
+
+        algorithm = self['algorithm'].native
+        algo_map = {
+            'sha1_plain_ecdsa': 'ecdsa',
+            'sha224_plain_ecdsa': 'ecdsa',
+            'sha256_plain_ecdsa': 'ecdsa',
+            'sha384_plain_ecdsa': 'ecdsa',
+            'sha512_plain_ecdsa': 'ecdsa'
+        }
+
+        if algorithm in algo_map:
+            return algo_map[algorithm]
+        raise ValueError('Signature algorithm not known for '.format(algorithm))
+
+    @property
+    def hashAlgo(self):
+        if not self.isPlain:
+            return super().hash_algo
+
+        algorithm = self['algorithm'].native
+        algo_map = {
+            'sha1_plain_ecdsa': 'sha1',
+            'sha224_plain_ecdsa': 'sha224',
+            'sha256_plain_ecdsa': 'sha256',
+            'sha384_plain_ecdsa': 'sha384',
+            'sha512_plain_ecdsa': 'sha512',
+        }
+
+        if algorithm in algo_map:
+            return algo_map[algorithm]
+        raise ValueError('Hash algorithm not known for '.format(algorithm))
 
 
 class AAPublicKey:
