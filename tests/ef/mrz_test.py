c27779b1bafd56285c2829eaf13e93da07e78701
smlu <smlu@s5.net>
2021-05-17 13:30:31 +0200

    [pymrtd] Add MRZ tests

---
+++ tests/ef/mrz_test.py
@@ -0,0 +1,85 @@
+import pytest
+from pymrtd import ef
+from datetime import date
+
+def _td_as_der(td) -> bytes:
+    td = "".join("{:02x}".format(ord(c)) for c in td)
+    td = '5F1F{:02x}{}'.format(int(len(td) /2), td)
+    return bytes(bytearray.fromhex(td))
+
+def test_mrz_parse():
+    # tv from ICAO 9303 part 10 A.2.1
+    tv  = _td_as_der("I<NLDXI85935F86999999990<<<<<<7208148F1108268NLD<<<<<<<<<<<4VAN<DER<STEEN<<MARIANNE<LOUISE")
+    mrz = ef.MachineReadableZone.load(tv)
+    assert mrz.dump()                   == tv
+    assert mrz.type                     == 'td1'
+    assert mrz.document_code            == 'I'
+    assert mrz.country                  == 'NLD'
+    assert mrz.document_number          == 'XI85935F8'
+    assert mrz['document_number_cd']    == '6'
+    assert mrz['optional_data_1']       == '999999990'
+    assert mrz.date_of_birth            == date( 1972, 8, 14 )
+    assert mrz['date_of_birth_cd']      == 8
+    assert mrz.gender                   == 'F'
+    assert mrz.date_of_expiry           == date( 2011, 8, 26 )
+    assert mrz['date_of_expiry_cd']     == 8
+    assert mrz.nationality              == 'NLD'
+    assert mrz['optional_data_2']       == ''
+    assert mrz['composite_cd']          == 4
+    assert mrz['name_identifiers']      == ( 'VAN DER STEEN', 'MARIANNE LOUISE' )
+    assert mrz.name                     == 'MARIANNE LOUISE'
+    assert mrz.surname                  == 'VAN DER STEEN'
+
+    # tv from ICAO 9303 part 11 Appendix D to Part 11 Section D.2
+    tv  = _td_as_der("I<UTOSTEVENSON<<PETER<JOHN<<<<<<<<<<D23145890<UTO3407127M95071227349<<<8")
+    mrz = ef.MachineReadableZone.load(tv)
+    assert mrz.dump()                   == tv
+    assert mrz.type                   == 'td2'
+    assert mrz.document_code            == 'I'
+    assert mrz.country                  == 'UTO'
+    assert mrz.document_number          == 'D23145890734'
+    assert mrz['document_number_cd']    == '9'
+    assert mrz['optional_data']         == ''
+    assert mrz.date_of_birth            == date( 1934, 7, 12 )
+    assert mrz['date_of_birth_cd']      == 7
+    assert mrz.gender                   == 'M'
+    assert mrz.date_of_expiry           == date( 1995, 7, 12 )
+    assert mrz['date_of_expiry_cd']     == 2
+    assert mrz.nationality              == 'UTO'
+    assert mrz['composite_cd']          == 8
+    assert mrz['name_identifiers']      == ( 'STEVENSON', 'PETER JOHN' )
+    assert mrz.name                     == 'PETER JOHN'
+    assert mrz.surname                  == 'STEVENSON'
+
+    # tv from ICAO 9303 part 4 Appendix B To Part 4
+    tv  = _td_as_der("P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<L898902C36UTO7408122F1204159ZE184226B<<<<<10")
+    mrz = ef.MachineReadableZone.load(tv)
+    assert mrz.dump()                   == tv
+    assert mrz.type                     == 'td3'
+    assert mrz.document_code            == 'P'
+    assert mrz.country                  == 'UTO'
+    assert mrz.document_number          == 'L898902C3'
+    assert mrz['document_number_cd']    == '6'
+    assert mrz['optional_data']         == 'ZE184226B'
+    assert mrz['optional_data_cd']      == 1
+    assert mrz.date_of_birth            == date( 1974, 8, 12 )
+    assert mrz['date_of_birth_cd']      == 2
+    assert mrz.gender                   == 'F'
+    assert mrz.date_of_expiry           == date( 2012, 4, 15 )
+    assert mrz['date_of_expiry_cd']     == 9
+    assert mrz.nationality              == 'UTO'
+    assert mrz['composite_cd']          == 0
+    assert mrz['name_identifiers']      == ( 'ERIKSSON', 'ANNA MARIA' )
+    assert mrz.name                     == 'ANNA MARIA'
+    assert mrz.surname                  == 'ERIKSSON'
+
+    with pytest.raises(TypeError, match="encoded_data must be a byte string, not str"):
+        ef.MachineReadableZone.load('')
+    with pytest.raises(ValueError, match="Insufficient data - 2 bytes requested but only 0 available"):
+        ef.MachineReadableZone.load(bytes())
+    with pytest.raises(ValueError, match="Unknown MRZ type"):
+        ef.MachineReadableZone.load(_td_as_der(''))
+    with pytest.raises(ValueError, match="Unknown MRZ type"):
+        ef.MachineReadableZone.load(_td_as_der('P<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<'))
+    with pytest.raises(ValueError, match="Unknown MRZ type"):
+        ef.MachineReadableZone.load(_td_as_der('XP<UTOERIKSSON<<ANNA<MARIA<<<<<<<<<<<<<<<<<<<L898902C36UTO7408122F1204159ZE184226B<<<<<10'))
