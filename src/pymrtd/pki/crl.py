bc125d1a134ae007c1ea5c655c4beb4cb84ad5b4
smlu <smlu@s5.net>
2021-07-20 17:18:54 +0200

    [pymrtd] Add conformance check to CRL && Fix CRL signature verification

--- src/pymrtd/pki/crl.py
+++ src/pymrtd/pki/crl.py
@@ -45,6 +45,10 @@ class CertificateRevocationList(CertificateList):
         next_update = self['tbs_cert_list']['next_update'].native
         return next_update.replace(tzinfo=None)
 
+    @property
+    def authorityKey(self) -> bytes:
+        """Returns authority key of CRL issuer"""
+        return self.authority_key_identifier
 
     @property
     def signatureAlgorithm(self) -> str:
@@ -64,6 +68,57 @@ class CertificateRevocationList(CertificateList):
         fp = self.sha256.hex()
         return fp
 
-    def verify(self, issuer: CscaCertificate):
-        """ Function verifies if crl is signed by provided issuer CSCA """
-        verify_sig(issuer, self['tbs_cert_list'].dump(), self['signature'], self['signature_algorithm'])
+    def checkConformance(self) -> None:
+        """
+        Verifies that this CRL conform to ICAO 9303 part 12, section 7.1.4 doc.
+        An exception is risen if conformance check fails.
+        :raises CertificateRevocationListError: If conformance check fails.
+        """
+        self._require_crl_field('issuer')
+        self._require_crl_field('this_update')
+        self._require_crl_field('next_update')
+
+        CertificateRevocationList._require('country_name' in self.issuer.native, # ICAO 9303 part 12 section 7.1.1.1.1
+            "Issuer field is missing field 'country_name'"
+        )
+        cn = self.issuer.native['country_name']
+        CertificateRevocationList._require( len(cn) == 2, # ICAO 9303 part 12 section 7.1.1.1.1
+            "Invalid country name in issuer field: {}".format(cn)
+        )
+
+        # Check crl number
+        CertificateRevocationList._require( self.crl_number_value is not None,
+            "Missing extension 'cRLNumber' "
+        )
+        CertificateRevocationList._require( self.crl_number_value.native >= 0,
+            "CRL number is negative"
+        )
+
+        CertificateRevocationList._require( self.delta_crl_indicator_value is None,
+            "CRL is delta"
+        )
+
+    def verify(self, issuerCert: CscaCertificate, checkConformance = False) -> None:
+        """
+        Function verifies if CRL is signed by provided issuer CSCA.
+        :param issuerCert: The CSCA certificate which issued this CRL.
+        :param checkConformance: If true conformance check is performed prior to signature verification.
+        :raises CertificateRevocationListError: If conformance check fails or signature verification fails.
+        :raises *Exception: If there was a problem in the process before signature is fully verified.
+             See cert_utils.verify_sig
+        """
+        if checkConformance:
+            self.checkConformance()
+
+        if not verify_sig(issuerCert, self['tbs_cert_list'].dump(), self['signature'], self['signature_algorithm']):
+            raise CertificateRevocationListError("Signature verification failed")
+
+    @staticmethod
+    def _require(cond, message: str):
+        if not cond:
+            raise CertificateRevocationListError(message)
+
+    def _require_crl_field(self, field: str):
+        CertificateRevocationList._require(field in self,
+            "Missing required certificate field '{}'".format(field)
+        )
